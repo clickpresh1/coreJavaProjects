4d0ce7780ec5975130cc5086fbdd931acc4c9d84
clickpresh1 <88908336+clickpresh1@users.noreply.github.com>
2025-09-07 22:24:44 +0100

    Update DogBreed.java

--- DogBreed.java
+++ DogBreed.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class DogBreed {
     
     public static void main(String[] args) {
-        System.out.println("German Shephard");
-    }
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int num = Integer.valueOf(scanner.nextLine());
+        breed(num);
+
+        scanner.close();
+    } 
+
+    public static String breed(int num) {
+        (num == 1) ? "German Shephard" : "Other breeds";
 }
