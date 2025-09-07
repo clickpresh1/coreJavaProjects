8405a908375cff4be33f8f3be51ab1e2893e2a94
clickpresh1 <clickpresh1@gmail.com>
2025-09-07 22:59:31 +0100

    update

---
+++ DogBreed/DogBreed.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class DogBreed {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int num = Integer.valueOf(scanner.nextLine());
+        breed(num);
+        numState(breed(num));
+        scanner.close();
+    } 
+
+    public static String breed(int num) {
+        String result = (num == 1) ? "German Shepherd" : "Other breeds";
+        //System.out.println(result);
+        return result;
+    }  
+
+    public static int numState(String line) {
+        int result = (line.equals("German Shepherd")) ? 1 : -1;
+        System.out.println(result);
+        return result;
+    }
+    
+}
