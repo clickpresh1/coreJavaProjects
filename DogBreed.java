e7634109751400a96ac3bdf5e101a1520db9a6d6
clickpresh1 <clickpresh1@gmail.com>
2025-09-07 22:43:15 +0100

    update DogBreed.java

--- DogBreed.java
+++ DogBreed.java
@@ -7,11 +7,12 @@ public class DogBreed {
         System.out.print("Enter number: ");
         int num = Integer.valueOf(scanner.nextLine());
         breed(num);
-
         scanner.close();
     } 
 
     public static String breed(int num) {
-        (num == 1) ? "German Shephard" : "Other breeds";
-    {
+        String result = (num == 1) ? "German Shepherd" : "Other breeds";
+        System.out.println(result);
+        return result;
+    }
 }
